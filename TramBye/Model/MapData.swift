0035660d09d9b0b97e84ff8a8cf80d5495be8e99
dominika-kokowicz <dominika.kokowicz@gmail.com>
2021-03-26 20:14:36 +0100

    Adds model.

---
+++ TramBye/Model/MapData.swift
@@ -0,0 +1,14 @@
+//
+//  TramsData.swift
+//  TramBye
+//
+//  Created by Dominika Kokowicz on 15/03/2021.
+//
+
+import Foundation
+import CoreLocation
+
+struct MapData {
+    let coordinate: CLLocationCoordinate2D
+    let title: String?
+}
