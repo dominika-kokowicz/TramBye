f94bdd64b554e28d6a6b7867a463954d5c3aa730
dominika-kokowicz <dominika.kokowicz@gmail.com>
2021-04-06 19:06:25 +0200

    Moves logic to extension

--- TramBye/Controller/TramLinesViewController.swift
+++ TramBye/Controller/TramLinesViewController.swift
@@ -13,30 +13,6 @@ class TramLinesViewController: UIViewController, UICollectionViewDelegate, UICol
 
     @IBOutlet weak var tramLinesCollectionView: UICollectionView!
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.tramLines.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TramLineCell.reuseIdentifier, for: indexPath) as! TramLineCell
-        cell.tramLineCellButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        cell.configure(with: "\(viewModel.tramLines[indexPath.row])")
-        cell.tramLineCellButton.tag = viewModel.tramLines[indexPath.row]
-           return cell
-       }
-
-       @objc func buttonTapped(sender: UIButton?) {
-        guard let tag = sender?.tag else { return }
-        
-        if chosenTramLinesToTrack.contains(tag) {
-            chosenTramLinesToTrack.remove(tag)
-        } else {
-            chosenTramLinesToTrack.insert(tag)
-        }
-        
-       }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tramLinesCollectionView.delegate = self
@@ -53,6 +29,21 @@ class TramLinesViewController: UIViewController, UICollectionViewDelegate, UICol
 
 }
 
-
+extension TramLinesViewController {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.tramLines.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TramLineCell.reuseIdentifier, for: indexPath) as! TramLineCell
+        cell.tramLineCellButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        cell.configure(with: "\(viewModel.tramLines[indexPath.row])")
+        cell.tramLineCellButton.tag = viewModel.tramLines[indexPath.row]
+           return cell
+       }
+    
+ 
+}
 
 
